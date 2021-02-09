545557115c42b2aa06337706b691ec6f2bcf5e1a
johns <johns@192.168.0.13>
2021-02-09 10:13:10 -0500

    Changed Mod ID

--- forge-1.12-14.21.1.2443-mdk/src/main/java/pigman/mod/util/Reference.java
+++ forge-1.12-14.21.1.2443-mdk/src/main/java/pigman/mod/util/Reference.java
@@ -2,7 +2,7 @@ package pigman.mod.util;
 
 public class Reference {
 
-	public static final String MODID = "nether_villages";
+	public static final String MODID = "lukium";
 	public static final String NAME= "PigmanMod";
 	public static final String VERSION="1.0";
 	public static final String CLIENT = "pigman.mod.proxy.ClientProxy";
